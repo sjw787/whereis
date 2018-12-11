900a92bcfb6fdbdcd9cbf2ac7c1f73914dae780a
sjw787 <swylock@albany.edu>
2018-12-11 00:05:21 -0500

    added build dir

---
+++ build/precache-manifest.88346b81520c9164f4d5c914534f4c78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e0a8aeb3b370d718f47e",
+    "url": "/static/js/main.e0a8aeb3.chunk.js"
+  },
+  {
+    "revision": "b1aa72b498d3655811c6",
+    "url": "/static/js/1.b1aa72b4.chunk.js"
+  },
+  {
+    "revision": "e0a8aeb3b370d718f47e",
+    "url": "/static/css/main.8dbec17a.chunk.css"
+  },
+  {
+    "revision": "55aece87d656174324a395069738cba1",
+    "url": "/index.html"
+  }
+];
